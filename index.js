f211ddf125114db42254c74de66aeb8ff0dcdd0d
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2022-10-02 13:20:41 +0700

    adding category API

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ mongoose
     );
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
 app.use("/api", require("./routes/app.routes"));
